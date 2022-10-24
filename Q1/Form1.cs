8601303313667f07d1386ad01a95b8b3b8886e43
建均 陳 <protosscarrier@gmail.com>
2022-10-24 17:14:07 +0800

    加入專案檔案。

---
+++ Q1/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Q1
+{
+	public partial class Form1 : Form
+	{
+		//Button click 時, 用 Label 顯示目前系統時間及問候語
+		//用MessageBox 顯示目前時間,格式為 yyyy/MM/dd HH:mm:ss
+		public Form1()
+		{
+			InitializeComponent();
+			lblShowTime.Text = String.Empty;
+		}
+
+		private void btnShowTime_Click(object sender, EventArgs e)
+		{
+			Greeting();
+			Display();
+		}
+		private DateTime dt = DateTime.Now;
+		private string greeting = String.Empty;
+		private void Greeting()
+		{
+			DateTime noon = DateTime.Today.AddHours(12);
+			DateTime night = noon.AddHours(6);
+
+			if (dt > night)
+			{
+				greeting = "晚安";
+			}
+			else if (dt > noon)
+			{
+				greeting = "午安";
+			}
+			else
+			{
+				greeting = "早安";
+			}
+		}
+		private void Display()
+		{
+			lblShowTime.Text = dt.ToString("現在時間: yyyy/MM/dd HH:mm:ss ") + greeting;
+
+			MessageBox.Show(dt.ToString("現在時間: yyyy/MM/dd HH:mm:ss"));
+		}
+	}
+}
---
+++ Q2/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Q2
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.lblShowTime = new System.Windows.Forms.Label();
+			this.btnShowTime = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// lblShowTime
+			// 
+			this.lblShowTime.AutoSize = true;
+			this.lblShowTime.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.lblShowTime.Location = new System.Drawing.Point(52, 149);
+			this.lblShowTime.Name = "lblShowTime";
+			this.lblShowTime.Size = new System.Drawing.Size(135, 31);
+			this.lblShowTime.TabIndex = 3;
+			this.lblShowTime.Text = "ShowTime";
+			// 
+			// btnShowTime
+			// 
+			this.btnShowTime.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.btnShowTime.Location = new System.Drawing.Point(160, 36);
+			this.btnShowTime.Name = "btnShowTime";
+			this.btnShowTime.Size = new System.Drawing.Size(196, 78);
+			this.btnShowTime.TabIndex = 2;
+			this.btnShowTime.Text = "顯示時間";
+			this.btnShowTime.UseVisualStyleBackColor = true;
+			this.btnShowTime.Click += new System.EventHandler(this.btnShowTime_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(517, 266);
+			this.Controls.Add(this.lblShowTime);
+			this.Controls.Add(this.btnShowTime);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label lblShowTime;
+		private System.Windows.Forms.Button btnShowTime;
+	}
+}
+
